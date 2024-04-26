ad989abce444d6b8e9e3963e3e1a0a9b41b16847
NeneNafe <nenechiwaya@gmail.com>
2024-04-26 14:06:21 +0200

    Corrections

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- creates a table users whit the following cndtns
 -- id must be int, email must be str, name must be str
-CREATE TABLE IF NOT EXISTS 'users' (
-    'id' INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    'email' VARCHAR(255) NOT NULL UNIQUE,
-    'name' VARCHAR(255)
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255)
 );
